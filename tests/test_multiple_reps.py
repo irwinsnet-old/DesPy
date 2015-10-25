666879008221d89cfb7cacbe1d3f00e102896de2
unknown <stacy.irwin@outlook.com>
2015-10-25 21:02:42 +0800

    Starting on multiple reps test.

---
+++ tests/test_multiple_reps.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 25, 2015
+
+@author: stacy.irwin
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def test_reps(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_reps']
+    unittest.main()
